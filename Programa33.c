d19ce16af7b5c5fcd42e0cc97c1e1527cd1772b8
Aram <159743513+GmzCrz@users.noreply.github.com>
2024-10-11 13:29:19 -0600

    Programa33.c

---
+++ Programa33.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main ()
+  {
+    char *ap, c = 'a';
+    ap = &c; 
+    printf("Carácter: %c\n",*ap);
+    printf("Código ASCII: %d\n",*ap); 
+    printf("Dirección de memoria: %d\n",ap);
+  }
